d7e156235d2f20dfefafd67149ad7692a535a21f
Gnaneshwar <gnaneshwarmareda@gmail.com>
2024-12-16 11:36:35 +0530

    OTP Service Done

--- backend/models/alumni.js
+++ backend/models/alumni.js
@@ -3,10 +3,8 @@ const mongoose = require("mongoose");
 const alumniSchema = new mongoose.Schema({
   Id: { type: String, required: true },
   password: { type: String, required: true },
-  role: { type: String, default: "alumni", required: true },
   name: { type: String, required: true },
   personalEmail: { type: String, required: true },
-  universityEmail: { type: String, required: true },
   mobile: { type: String, required: true },
   graduationYear: { type: String, required: true },
   fieldOfStudy: { type: String, required: true },
--- backend/routes/register.js
+++ backend/routes/register.js
@@ -1,66 +1,53 @@
 const Student = require("../models/students.js");
-const Alumni = require("../models/alumni.js");
 
 const express = require("express");
 const router = express.Router();
-
 const bcrypt = require("bcrypt");
-const generateJwtToken = require("../utils/generateJwtToken");
 
-router.post("/user", async (req, res) => {
+const generateJwtToken = require("../utils/generateJwtToken");
+const { generateOTP } = require("../utils/otpCenter.js");
+const { verifyOTP } = require("../utils/otpCenter.js");
+
+router.post("/generateotp", generateOTP);
+router.post("/user", verifyOTP, async (req, res) => {
   try {
     const {
+      universityEmail,
       userId,
       password,
-      role,
       firstName,
       lastName,
       personalEmail,
-      universityEmail,
       mobile,
+      yearOfStudy,
       graduationYear,
       fieldOfStudy,
     } = req.body;
+
     const name = firstName + " " + lastName;
 
     // Check if user already exists in the database
-    const isStudent = await Student.findOne({ Id: userId });
-    const isAlumni = await Alumni.findOne({ Id: userId });
+    const isExistingUser = await Student.findOne({ Id: userId });
 
-    if (isStudent || isAlumni) {
+    if (isExistingUser) {
       return res.status(300).json({ message: "User Already Exists." });
     }
 
     // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
-
-    if (role === "student") {
-      const newUser = new Student({
-        Id: userId,
-        password: hashedPassword,
-        role,
-        name,
-        personalEmail,
-        universityEmail,
-        mobile,
-        graduationYear,
-        fieldOfStudy,
-      });
-      await newUser.save();
-    } else if (role === "alumni") {
-      const newUser = new Alumni({
-        Id: userId,
-        password: hashedPassword,
-        role,
-        name,
-        personalEmail,
-        universityEmail,
-        mobile,
-        graduationYear,
-        fieldOfStudy,
-      });
-      await newUser.save();
-    }
+    const newUser = new Student({
+      Id: userId,
+      password: hashedPassword,
+      role,
+      name,
+      personalEmail,
+      universityEmail,
+      mobile,
+      yearOfStudy,
+      graduationYear,
+      fieldOfStudy,
+    });
+    await newUser.save();
 
     // Generate JWT token
     const jwtToken = generateJwtToken(userId, role);
@@ -70,26 +57,5 @@ router.post("/user", async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 });
-
-// router.post("/alumni", async (req, res) => {
-//   try {
-//     const { userId, name, password, email, domain_email } = req.body;
-//     const existingUserId = await Alumni.findOne({ userId });
-
-//     if (existingUserId) {
-//       return res.status(300).json({ message: "User Already Exists." });
-//     }
-
-//     // Hash the password
-//     const hashedPassword = await bcrypt.hash(password, 10);
-
-//     // Generate JWT token
-//     const token = generateJwtToken(userId, user.role);
-
-//     res.status(201).json({ message: "User created successfully", token });
-//   } catch (error) {
-//     res.status(400).json({ message: error.message });
-//   }
-// });
 
 module.exports = router;
